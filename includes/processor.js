0228be4e42dbd86a34afb4613acaceb8311becaa
TheUberKid <zekixu@gmail.com>
2017-08-04 22:29:06 -0700

    fixed issue where being unable to process replay would lead to server crash

--- includes/processor.js
+++ includes/processor.js
@@ -97,125 +97,132 @@ function runReplay(fname, pollPath, callback){
         // if valid gametype, check for duplicates
         var id = initdata.m_gameDescription.m_randomValue;
         db_Replay.count({Id: id}, function(err, count){
-          if(err){
-            logger.log('info', '[REPLAY] database count error: ' + err.message);
-            pollRes(pollPath, 0, fname);
-            callback();
-          } else if(count > 0){
-            pollRes(pollPath, 1, fname); // duplicate found
-            callback();
-          } else {
-
-            // otherwise, good to go. Read heavier objects
-            var scores = heroprotocol.get(heroprotocol.TRACKER_EVENTS, file, {
-              '_event' : ['NNet.Replay.Tracker.SScoreResultEvent']
-            });
-            const talents = heroprotocol.get(heroprotocol.TRACKER_EVENTS, file, {
-              'm_eventName' : ['EndOfGameTalentChoices']
-            });
-            if(!scores || !talents){
-              logger.log('info', '[REPLAY] tracker events processing error');
+          try {
+            if(err){
+              logger.log('info', '[REPLAY] database count error: ' + err.message);
               pollRes(pollPath, 0, fname);
               callback();
-              return 0;
-            }
-
-            // prepare for database entry
-            var tmp = scores[scores.length-1].m_instanceList;
-            scores = {};
-            for(var i in tmp) scores[tmp[i].m_name] = tmp[i].m_values;
-            var mvp = !!scores.EndOfMatchAwardMVPBoolean;
-
-            // add each player to database object
-            var players = [];
-            for(var i=0; i<10; i++){
-              var p = details.m_playerList[i];
-              var t = talents[i].m_stringData;
-              var player = {
-                Name: p.m_name,
-                BattleTag: getBattleTags(file, p.m_name),
-                ToonId: p.m_toon.m_id,
-                AI: p.m_toon.m_id === 0,
-                Hero: initdata.m_lobbyState.m_slots[i].m_hero,
-                Team: p.m_teamId,
-                SoloKill: scores.SoloKill[i][0].m_value,
-                Assists: scores.Assists[i][0].m_value,
-                Deaths: scores.Deaths[i][0].m_value,
-                ExperienceContribution: scores.ExperienceContribution[i][0].m_value,
-                Healing: scores.Healing[i][0].m_value,
-                SelfHealing: scores.SelfHealing[i][0].m_value,
-                SiegeDamage: scores.SiegeDamage[i][0].m_value,
-                StructureDamage: scores.StructureDamage[i][0].m_value,
-                MinionDamage: scores.MinionDamage[i][0].m_value,
-                CreepDamage: scores.CreepDamage[i][0].m_value,
-                SummonDamage: scores.SummonDamage[i][0].m_value,
-                HeroDamage: scores.HeroDamage[i][0].m_value,
-                DamageTaken: scores.DamageTaken[i][0].m_value,
-                MercCampCaptures: scores.MercCampCaptures[i][0].m_value,
-                TimeSpentDead: scores.TimeSpentDead[i][0].m_value,
-                TimeCCdEnemyHeroes: scores.TimeCCdEnemyHeroes[i][0].m_value,
-                Tier1Talent: t[3] ? t[3].m_value : '',
-                Tier2Talent: t[4] ? t[4].m_value : '',
-                Tier3Talent: t[5] ? t[5].m_value : '',
-                Tier4Talent: t[6] ? t[6].m_value : '',
-                Tier5Talent: t[7] ? t[7].m_value : '',
-                Tier6Talent: t[8] ? t[8].m_value : '',
-                Tier7Talent: t[9] ? t[9].m_value : '',
-                MVP: mvp ? scores.EndOfMatchAwardMVPBoolean[i][0].m_value === 1 : false
-              }
-              if(scores.TimeSilencingEnemyHeroes){
-                Object.assign(player, {
-                  TimeSilencingEnemyHeroes: scores.TimeSilencingEnemyHeroes[i][0].m_value,
-                  TimeRootingEnemyHeroes: scores.TimeRootingEnemyHeroes[i][0].m_value,
-                  TimeStunningEnemyHeroes: scores.TimeStunningEnemyHeroes[i][0].m_value,
-                  ClutchHealsPerformed: scores.ClutchHealsPerformed[i][0].m_value,
-                  EscapesPerformed: scores.EscapesPerformed[i][0].m_value,
-                  VengeancesPerformed: scores.VengeancesPerformed[i][0].m_value,
-                  OutnumberedDeaths: scores.OutnumberedDeaths[i][0].m_value,
-                  TeamfightEscapesPerformed: scores.TeamfightEscapesPerformed[i][0].m_value,
-                  TeamfightHealingDone: scores.TeamfightHealingDone[i][0].m_value,
-                  TeamfightDamageTaken: scores.TeamfightDamageTaken[i][0].m_value,
-                  TeamfightHeroDamage: scores.TeamfightHeroDamage[i][0].m_value,
-                  HighestKillStreak: scores.HighestKillStreak[i][0].m_value
-                });
-              }
-              players.push(player);
-            }
-
-            var lvl = scores.TeamLevel ?
-              [scores.TeamLevel[0][0].m_value, scores.TeamLevel[9][0].m_value] :
-              [scores.Level[0][0].m_value, scores.Level[9][0].m_value];
-            var replay = db_Replay({
-              Id: id,
-              Build: header.m_version.m_build,
-              Region: details.m_playerList[0].m_toon.m_region,
-              SubmittedBy: polls[pollPath].ultoken ? polls[pollPath].ultoken : 0,
-              MapName: details.m_title,
-              GameType: gametype,
-              WinningTeam: talents[0].m_stringData[1].m_value === 'Win' ? 0 : 1,
-              Team0Level: lvl[0],
-              Team1Level: lvl[1],
-              GameLength: header.m_elapsedGameLoops,
-              TimePlayed: new Date(details.m_timeUTC / 10000 - 11644473600000).getTime(),
-              TimeSubmitted: Date.now(),
-              Players: players
-            });
-
-            // save to database
-            replay.save(function(err){
-              if(err){
-                if(err.message.indexOf('duplicate') > -1){
-                  pollRes(pollPath, 1, fname);
-                } else {
-                  pollRes(pollPath, 0, fname);
-                  logger.log('info', '[REPLAY] save error: ' + err.message);
-                }
-              } else {
-                pollRes(pollPath, gametype, fname);
-              }
+            } else if(count > 0){
+              pollRes(pollPath, 1, fname); // duplicate found
               callback();
-            });
+            } else {
 
+              // otherwise, good to go. Read heavier objects
+              var scores = heroprotocol.get(heroprotocol.TRACKER_EVENTS, file, {
+                '_event' : ['NNet.Replay.Tracker.SScoreResultEvent']
+              });
+              const talents = heroprotocol.get(heroprotocol.TRACKER_EVENTS, file, {
+                'm_eventName' : ['EndOfGameTalentChoices']
+              });
+              if(!scores || !talents){
+                logger.log('info', '[REPLAY] tracker events processing error');
+                pollRes(pollPath, 0, fname);
+                callback();
+                return 0;
+              }
+
+              // prepare for database entry
+              var tmp = scores[scores.length-1].m_instanceList;
+              scores = {};
+              for(var i in tmp) scores[tmp[i].m_name] = tmp[i].m_values;
+              var mvp = !!scores.EndOfMatchAwardMVPBoolean;
+
+              // add each player to database object
+              var players = [];
+              for(var i=0; i<10; i++){
+                var p = details.m_playerList[i];
+                var t = talents[i].m_stringData;
+                var player = {
+                  Name: p.m_name,
+                  BattleTag: getBattleTags(file, p.m_name),
+                  ToonId: p.m_toon.m_id,
+                  AI: p.m_toon.m_id === 0,
+                  Hero: initdata.m_lobbyState.m_slots[i].m_hero,
+                  Team: p.m_teamId,
+                  SoloKill: scores.SoloKill[i][0].m_value,
+                  Assists: scores.Assists[i][0].m_value,
+                  Deaths: scores.Deaths[i][0].m_value,
+                  ExperienceContribution: scores.ExperienceContribution[i][0].m_value,
+                  Healing: scores.Healing[i][0].m_value,
+                  SelfHealing: scores.SelfHealing[i][0].m_value,
+                  SiegeDamage: scores.SiegeDamage[i][0].m_value,
+                  StructureDamage: scores.StructureDamage[i][0].m_value,
+                  MinionDamage: scores.MinionDamage[i][0].m_value,
+                  CreepDamage: scores.CreepDamage[i][0].m_value,
+                  SummonDamage: scores.SummonDamage[i][0].m_value,
+                  HeroDamage: scores.HeroDamage[i][0].m_value,
+                  DamageTaken: scores.DamageTaken[i][0].m_value,
+                  MercCampCaptures: scores.MercCampCaptures[i][0].m_value,
+                  TimeSpentDead: scores.TimeSpentDead[i][0].m_value,
+                  TimeCCdEnemyHeroes: scores.TimeCCdEnemyHeroes[i][0].m_value,
+                  Tier1Talent: t[3] ? t[3].m_value : '',
+                  Tier2Talent: t[4] ? t[4].m_value : '',
+                  Tier3Talent: t[5] ? t[5].m_value : '',
+                  Tier4Talent: t[6] ? t[6].m_value : '',
+                  Tier5Talent: t[7] ? t[7].m_value : '',
+                  Tier6Talent: t[8] ? t[8].m_value : '',
+                  Tier7Talent: t[9] ? t[9].m_value : '',
+                  MVP: mvp ? scores.EndOfMatchAwardMVPBoolean[i][0].m_value === 1 : false
+                }
+                if(scores.TimeSilencingEnemyHeroes){
+                  Object.assign(player, {
+                    TimeSilencingEnemyHeroes: scores.TimeSilencingEnemyHeroes[i][0].m_value,
+                    TimeRootingEnemyHeroes: scores.TimeRootingEnemyHeroes[i][0].m_value,
+                    TimeStunningEnemyHeroes: scores.TimeStunningEnemyHeroes[i][0].m_value,
+                    ClutchHealsPerformed: scores.ClutchHealsPerformed[i][0].m_value,
+                    EscapesPerformed: scores.EscapesPerformed[i][0].m_value,
+                    VengeancesPerformed: scores.VengeancesPerformed[i][0].m_value,
+                    OutnumberedDeaths: scores.OutnumberedDeaths[i][0].m_value,
+                    TeamfightEscapesPerformed: scores.TeamfightEscapesPerformed[i][0].m_value,
+                    TeamfightHealingDone: scores.TeamfightHealingDone[i][0].m_value,
+                    TeamfightDamageTaken: scores.TeamfightDamageTaken[i][0].m_value,
+                    TeamfightHeroDamage: scores.TeamfightHeroDamage[i][0].m_value,
+                    HighestKillStreak: scores.HighestKillStreak[i][0].m_value
+                  });
+                }
+                players.push(player);
+              }
+
+              var lvl = scores.TeamLevel ?
+                [scores.TeamLevel[0][0].m_value, scores.TeamLevel[9][0].m_value] :
+                [scores.Level[0][0].m_value, scores.Level[9][0].m_value];
+              var replay = db_Replay({
+                Id: id,
+                Build: header.m_version.m_build,
+                Region: details.m_playerList[0].m_toon.m_region,
+                SubmittedBy: polls[pollPath].ultoken ? polls[pollPath].ultoken : 0,
+                MapName: details.m_title,
+                GameType: gametype,
+                WinningTeam: talents[0].m_stringData[1].m_value === 'Win' ? 0 : 1,
+                Team0Level: lvl[0],
+                Team1Level: lvl[1],
+                GameLength: header.m_elapsedGameLoops,
+                TimePlayed: new Date(details.m_timeUTC / 10000 - 11644473600000).getTime(),
+                TimeSubmitted: Date.now(),
+                Players: players
+              });
+
+              // save to database
+              replay.save(function(err){
+                if(err){
+                  if(err.message.indexOf('duplicate') > -1){
+                    pollRes(pollPath, 1, fname);
+                  } else {
+                    pollRes(pollPath, 0, fname);
+                    logger.log('info', '[REPLAY] save error: ' + err.message);
+                  }
+                } else {
+                  pollRes(pollPath, gametype, fname);
+                }
+                callback();
+              });
+
+            }
+          } catch(err) {
+            // encountered error classifying or storing data
+            logger.log('info', '[REPLAY] classification error: ' + err.message);
+            pollRes(pollPath, 0, fname);
+            callback();
           }
         });
 
@@ -228,7 +235,7 @@ function runReplay(fname, pollPath, callback){
 
     }
   } catch(err) {
-    
+
     // encountered error classifying or storing data
     logger.log('info', '[REPLAY] classification error: ' + err.message);
     pollRes(pollPath, 0, fname);
